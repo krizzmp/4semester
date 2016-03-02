28a3ad0b9d8b6a2a96222d4a5a7e138d0f6d1dec
krizzmp <krizzmp@gmail.com>
2016-03-03 00:05:25 +0100

    sets up basic structure

---
+++ Core/src/main/Game.java
@@ -0,0 +1,17 @@
+package main;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class Game extends com.badlogic.gdx.Game {
+
+    @Override
+    public void create() {
+        // TODO: 02/03/16 add initializing logic, such as setting the first screen
+
+        //something like this:
+        //setScreen(new StartScreen());
+    }
+
+}
