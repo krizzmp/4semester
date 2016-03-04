0aa2962c6fbe744262b34743a7b99c99e3001949
Klaus Reche Riisom <Klrii14@student.sdu.dk>
2016-03-04 14:33:51 +0100

    LibGDX setup added to Installer

--- CrystalDefence/Core/src/main/java/dk/sdu/group5/core/Installer.java
+++ CrystalDefence/Core/src/main/java/dk/sdu/group5/core/Installer.java
@@ -1,17 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dk.sdu.group5.core;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.openide.modules.ModuleInstall;
 
 public class Installer extends ModuleInstall {
 
     @Override
     public void restored() {
-        // TODO
-    }
 
-}
+       LwjglApplicationConfiguration cfg
+               = new LwjglApplicationConfiguration();
+       cfg.title = "Crystal Defence";
+       cfg.width = 500;
+       cfg.height = 400;
+       cfg.useGL30 = false;
+       cfg.resizable = false;
+   }
+    }
