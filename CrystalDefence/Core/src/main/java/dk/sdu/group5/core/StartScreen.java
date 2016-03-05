87ca422ce9167574035ece4ce929b152e46b0a1b
krizzmp <krizzmp@gmail.com>
2016-03-05 11:45:22 +0100

    makes start screen prettier xD closes #33

--- CrystalDefence/Core/src/main/java/dk/sdu/group5/core/StartScreen.java
+++ CrystalDefence/Core/src/main/java/dk/sdu/group5/core/StartScreen.java
@@ -19,7 +19,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-class StartScreen implements Screen, InputProcessor {
+class StartScreen implements Screen {
 
     private SpriteBatch batch;
     private BitmapFont font;
@@ -51,7 +51,7 @@ class StartScreen implements Screen, InputProcessor {
         table.setFillParent(true);
         stage.addActor(table);
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle(skin.getDrawable("button_01"), skin.getDrawable("button_01"), skin.getDrawable("button_01"), font);
-        final TextButton button = new TextButton("Click me", style);
+        final TextButton button = new TextButton("start game", style);
         table.add(button);
         button.addListener(new ClickListener(){
             @Override
@@ -98,102 +98,4 @@ class StartScreen implements Screen, InputProcessor {
         stage.dispose();
     }
 
-    /**
-     * Called when a key was pressed
-     *
-     * @param keycode one of the constants in {@link Input.Keys}
-     * @return whether the input was processed
-     */
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    /**
-     * Called when a key was released
-     *
-     * @param keycode one of the constants in {@link Input.Keys}
-     * @return whether the input was processed
-     */
-    @Override
-    public boolean keyUp(int keycode) {
-        if (keycode == Input.Keys.ENTER) {
-            onEnter.run();
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Called when a key was typed
-     *
-     * @param character The character
-     * @return whether the input was processed
-     */
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    /**
-     * Called when the screen was touched or a mouse button was pressed. The button parameter will be {@link Input.Buttons#LEFT} on iOS.
-     *
-     * @param screenX The x coordinate, origin is in the upper left corner
-     * @param screenY The y coordinate, origin is in the upper left corner
-     * @param pointer the pointer for the event.
-     * @param button  the button
-     * @return whether the input was processed
-     */
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    /**
-     * Called when a finger was lifted or a mouse button was released. The button parameter will be {@link Input.Buttons#LEFT} on iOS.
-     *
-     * @param screenX
-     * @param screenY
-     * @param pointer the pointer for the event.
-     * @param button  the button   @return whether the input was processed
-     */
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    /**
-     * Called when a finger or the mouse was dragged.
-     *
-     * @param screenX
-     * @param screenY
-     * @param pointer the pointer for the event.  @return whether the input was processed
-     */
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    /**
-     * Called when the mouse was moved without any buttons being pressed. Will not be called on iOS.
-     *
-     * @param screenX
-     * @param screenY
-     * @return whether the input was processed
-     */
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    /**
-     * Called when the mouse wheel was scrolled. Will not be called on iOS.
-     *
-     * @param amount the scroll amount, -1 or 1 depending on the direction the wheel was scrolled.
-     * @return whether the input was processed.
-     */
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
-    }
 }
