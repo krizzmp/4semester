da818a80d537fde0fb0473424089179d36b05bb1
krizzmp <krizzmp@gmail.com>
2016-03-09 15:52:36 +0100

    loads processes in GameScreen and calls update on them

--- CrystalDefence/Core/src/main/java/dk/sdu/group5/core/GameScreen.java
+++ CrystalDefence/Core/src/main/java/dk/sdu/group5/core/GameScreen.java
@@ -8,14 +8,18 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import dk.sdu.group5.common.data.World;
+import dk.sdu.group5.common.services.IGameProcess;
 import org.openide.util.Lookup;
+
+import java.util.Collection;
 
 
 class GameScreen implements Screen {
     private SpriteBatch batch;
     private BitmapFont font;
-    private World currentWorld ;
-        
+    private World world ;
+    private Collection<? extends IGameProcess> processes;
+
     /**
      * Called when this screen becomes the current screen for a {@link Game}.
      */
@@ -24,6 +28,8 @@ class GameScreen implements Screen {
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.CYAN);
+        processes = Lookup.getDefault().lookupAll(IGameProcess.class);
+        world = new World();
     }
 
     /**
@@ -35,14 +41,15 @@ class GameScreen implements Screen {
     public void render(float delta) {
         // TODO: 03/03/16 add game rendering
 
+        //update entities
+        processes.forEach((p)->p.update(world,delta));
+
+        //render
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         font.draw(batch, "the game is now running.", 150, 220);
         batch.end();
-
-        // I don't understand this:
-        //currentWorld =  Lookup.getDefault().lookup(World.class);
     }
     
     /**
@@ -50,8 +57,8 @@ class GameScreen implements Screen {
      */
 
     /**
-     * @param width
-     * @param height
+     * @param width the width of the window
+     * @param height the height of the window
      * @see ApplicationListener#resize(int, int)
      */
     @Override
