c4876fa9f51a7ec4ab7e5052e42503cab8ff9296
Minerr <jack.corp@hotmail.com>
2016-03-16 21:49:15 +0100

    Added Weapon module, and added WeaponGameProcessor to it

---
+++ CrystalDefence/Weapon/src/main/java/dk/sdu/group5/weapon/WeaponGameProcessor.java
@@ -0,0 +1,36 @@
+
+package dk.sdu.group5.weapon;
+
+import dk.sdu.group5.common.data.World;
+import dk.sdu.group5.common.services.IGameProcess;
+import org.openide.util.lookup.ServiceProvider;
+
+@ServiceProvider(service = IGameProcess.class)
+public class WeaponGameProcessor implements IGameProcess {
+
+    @Override
+    public void install() {
+
+    }
+
+    @Override
+    public void start(World world) {
+
+    }
+
+    @Override
+    public void update(World world, float delta) {
+
+    }
+
+    @Override
+    public void stop(World world) {
+
+    }
+
+    @Override
+    public void uninstall() {
+
+    }
+    
+}
