8bfed587c3d0963f8531a37d4d3fcfa36bebd700
Minerr <jack.corp@hotmail.com>
2016-03-17 15:27:48 +0100

    Added some dependencies Added bulletTexture.png Added some information to bullet in Weapon, also added player_shoot button to update

--- CrystalDefence/Weapon/src/main/java/dk/sdu/group5/weapon/WeaponGameProcessor.java
+++ CrystalDefence/Weapon/src/main/java/dk/sdu/group5/weapon/WeaponGameProcessor.java
@@ -1,6 +1,9 @@
 
 package dk.sdu.group5.weapon;
 
+import dk.sdu.group5.common.data.Entity;
+import dk.sdu.group5.common.data.EntityType;
+import dk.sdu.group5.common.data.GameKeys;
 import dk.sdu.group5.common.data.World;
 import dk.sdu.group5.common.services.IGameProcess;
 import org.openide.util.lookup.ServiceProvider;
@@ -8,6 +11,9 @@ import org.openide.util.lookup.ServiceProvider;
 @ServiceProvider(service = IGameProcess.class)
 public class WeaponGameProcessor implements IGameProcess {
 
+    private Entity bullet;
+    
+    
     @Override
     public void install() {
 
@@ -15,12 +21,16 @@ public class WeaponGameProcessor implements IGameProcess {
 
     @Override
     public void start(World world) {
-
+        bullet.setType(EntityType.BULLET);
+        bullet.setLives(1);
+        bullet.setTexture("bulletTexture.png");
+        
     }
 
     @Override
     public void update(World world, float delta) {
-
+        if(GameKeys.getInstance().player_shoot.getKeyState()) {
+        }
     }
 
     @Override
