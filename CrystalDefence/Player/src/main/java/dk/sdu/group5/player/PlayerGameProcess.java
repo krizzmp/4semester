008a75e673f03e0342bc56ea00225374f6222a9b
krizzmp <krizzmp@gmail.com>
2016-03-17 13:44:21 +0100

    makes player movement use the speed from Entity

--- CrystalDefence/Player/src/main/java/dk/sdu/group5/player/PlayerGameProcess.java
+++ CrystalDefence/Player/src/main/java/dk/sdu/group5/player/PlayerGameProcess.java
@@ -13,7 +13,6 @@ import java.util.logging.Logger;
 @ServiceProvider(service = IGameProcess.class)
 public class PlayerGameProcess implements IGameProcess {
     private Entity player;
-    private int speed;
 
     @Override
     public void install() {
@@ -28,7 +27,7 @@ public class PlayerGameProcess implements IGameProcess {
         player.setX(250);
         player.setY(250);
         player.setTexture("playerTexture.png");
-        speed = 60;                                // Should probably be implementet into Entity
+        player.setSpeed(60);
         player.addProperty("collidable");
         player.addProperty("tangible");
         player.addProperty("damageable");
@@ -39,19 +38,18 @@ public class PlayerGameProcess implements IGameProcess {
     public void update(World world, float delta) {
         //Player Movement
         GameKeys gameKeys = GameKeys.getInstance();
-        boolean upState = gameKeys.player_movement_up.getKeyState();
-        if (upState) {
-            float v = player.getY() + speed * delta;
-            player.setY(v);
+        float playerSpeed = player.getSpeed();
+        if (gameKeys.player_movement_up.getKeyState()) {
+            player.setY(player.getY() + playerSpeed * delta);
         }
         if (gameKeys.player_movement_down.getKeyState()) {
-            player.setY(player.getY() - speed * delta);
+            player.setY(player.getY() - playerSpeed * delta);
         }
         if (gameKeys.player_movement_left.getKeyState()) {
-            player.setX(player.getX() - speed * delta);
+            player.setX(player.getX() - playerSpeed * delta);
         }
         if (gameKeys.player_movement_right.getKeyState()) {
-            player.setX(player.getX() + speed * delta);
+            player.setX(player.getX() + playerSpeed * delta);
         }
 
 
