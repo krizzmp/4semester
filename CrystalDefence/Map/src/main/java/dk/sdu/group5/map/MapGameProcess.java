bd30ddea99aadd5117f932e21dbbb5c0e643d59c
Nousla <sirtheorist@gmail.com>
2016-04-29 12:13:20 +0200

    Changed Map to place environment entities at predefined locations, as well as a tower in the center.

--- CrystalDefence/Map/src/main/java/dk/sdu/group5/map/MapGameProcess.java
+++ CrystalDefence/Map/src/main/java/dk/sdu/group5/map/MapGameProcess.java
@@ -2,7 +2,6 @@ package dk.sdu.group5.map;
 
 import dk.sdu.group5.common.data.Entity;
 import dk.sdu.group5.common.data.EntityType;
-import dk.sdu.group5.common.data.Posf2d;
 import dk.sdu.group5.common.data.World;
 import dk.sdu.group5.common.data.collision.AABB;
 import dk.sdu.group5.common.data.collision.SquareCollider;
@@ -11,12 +10,10 @@ import org.openide.util.lookup.ServiceProvider;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @ServiceProvider(service = IGameProcess.class)
 public class MapGameProcess implements IGameProcess {
     private List<Entity> environment;
-    private int MAX_AMOUNT = 10;
 
     @Override
     public void install() {
@@ -26,71 +23,43 @@ public class MapGameProcess implements IGameProcess {
     public void start(World world) {
         environment = new ArrayList<>();
 
-        Random rng = new Random();
+        world.setBackgroundTexturePath("mapTexture.png");
 
-        int columnCount = 800 / 32;
-        int rowCount = 400 / 32;
-        float offsetWidth = 800f / 32f;
-        float offsetHeight = 400f / 32f;
+        environment.add(createEnvironmentEntity(448, 197));
+        environment.add(createEnvironmentEntity(216, 19));
+        environment.add(createEnvironmentEntity(288, 222));
+        environment.add(createEnvironmentEntity(337, 336));
+        // TODO: 26/04/16 Get actual width and height.
+        environment.add(createTowerEntity(350, 280));
 
-        boolean[][] usedTiles = new boolean[columnCount][rowCount];
-
-        int currentAmount = 0;
-        while (currentAmount < MAX_AMOUNT) {
-            Posf2d pos = getNextPosition(rng, usedTiles, columnCount, rowCount);
-            environment.add(createEnvironmentEntity(pos.getX() * 32 + offsetWidth, pos.getY() * 32 + offsetHeight));
-            currentAmount++;
+        for (Entity environment1 : environment) {
+            world.addEntity(environment1);
         }
-//        for (int i = 0; i < columnCount; i++) {
-//            for (int j = 0; j < rowCount; j++) {
-//                if(i >= columnCount/2 - 1 && i <= columnCount/2 + 1
-//                        && j >= rowCount/2 - 1 && j <= rowCount/2 + 1){
-//                    continue;
-//                }
-//
-//                if(rng.nextFloat() >= 0.95){
-//                    environment.add(createEnvironmentEntity(i * 32 + offsetWidth, j * 32 + offsetHeight));
-//                    currentAmount++;
-//                }
-//
-//                if(currentAmount == MAX_AMOUNT){
-//                    break;
-//                }
-//            }
-//            if(currentAmount == MAX_AMOUNT){
-//                break;
-//            }
-//        }
-
-        for (int i = 0; i < environment.size(); i++) {
-            world.addEntity(environment.get(i));
-        }
-    }
-
-    private Posf2d getNextPosition(Random rng, boolean[][] usedTiles, int columnCount, int rowCount) {
-        int candidateX = -1, candidateY = -1, attempts = 0;
-        do {
-            candidateX = rng.nextInt(columnCount);
-            candidateY = rng.nextInt(rowCount);
-            attempts++;
-        } while (usedTiles[candidateX][candidateY] || attempts != 100);
-
-        if (candidateX == -1 || candidateY == -1) {
-            return null;
-        }
-
-        return new Posf2d(candidateX, candidateY);
     }
 
     private Entity createEnvironmentEntity(float x, float y) {
         Entity entity = new Entity();
         entity.setType(EntityType.ENVIRONMENT);
         entity.setCollider(new SquareCollider(false, new AABB(-16, -16, 32, 32)));
-        entity.setTexture("gridPattern.png");
+        entity.setTexturePath("gridPattern.png");
         entity.addProperty("tangible");
         entity.addProperty("collidable");
         entity.addProperty("static");
-        // TODO: 26/04/16 Get actual width and height.
+        entity.setX(x);
+        entity.setY(y);
+        return entity;
+    }
+
+    private Entity createTowerEntity(float x, float y) {
+        Entity entity = new Entity();
+        entity.setType(EntityType.TOWER);
+        entity.setHealth(3);
+        entity.setTexturePath("towerTexture.png");
+        entity.setCollider(new SquareCollider(false, new AABB(-16, -16, 32, 32)));
+        entity.addProperty("tangible");
+        entity.addProperty("collidable");
+        entity.addProperty("static");
+        entity.addProperty("damageable");
         entity.setX(x);
         entity.setY(y);
         return entity;
@@ -102,6 +71,9 @@ public class MapGameProcess implements IGameProcess {
 
     @Override
     public void stop(World world) {
+        environment.stream().forEach((entity) -> {
+            world.removeEntity(entity);
+        });
     }
 
     @Override
--- CrystalDefence/Tower/src/main/java/dk/sdu/group5/tower/TowerGameProcess.java
+++ CrystalDefence/Tower/src/main/java/dk/sdu/group5/tower/TowerGameProcess.java
@@ -3,14 +3,14 @@ package dk.sdu.group5.tower;
 import dk.sdu.group5.common.data.Entity;
 import dk.sdu.group5.common.data.EntityType;
 import dk.sdu.group5.common.data.World;
-import dk.sdu.group5.common.data.collision.AABB;
-import dk.sdu.group5.common.data.collision.SquareCollider;
 import dk.sdu.group5.common.services.IGameProcess;
 import org.openide.util.lookup.ServiceProvider;
 
+import java.util.List;
+import java.util.Optional;
+
 @ServiceProvider(service = IGameProcess.class)
 public class TowerGameProcess implements IGameProcess {
-    private Entity tower;
 
     @Override
     public void install() {
@@ -19,31 +19,23 @@ public class TowerGameProcess implements IGameProcess {
 
     @Override
     public void start(World world) {
-        tower = new Entity();
-        tower.setType(EntityType.TOWER);
-        tower.setHealth(3);
-        tower.setX(350);
-        tower.setY(280);
-        tower.setTexturePath("towerTexture.png");
-        tower.setCollider(new SquareCollider(false, new AABB(-16, -16, 32, 32)));
-        tower.addProperty("tangible");
-        tower.addProperty("collidable");
-        tower.addProperty("static");
-        tower.addProperty("damageable");
-        world.addEntity(tower);
     }
 
     @Override
     public void update(World world, float delta) {
-        if(tower.getHealth() < 0){
-            world.setGameover(true);
-        }
+        getFirstTower(world.getEntities()).ifPresent(t -> {
+            if (t.getHealth() < 0) {
+                world.setGameover(true);
+            }
+        });
+    }
+
+    private Optional<Entity> getFirstTower(List<Entity> entities) {
+        return entities.stream().filter(e -> e.getType() == EntityType.TOWER).findFirst();
     }
 
     @Override
     public void stop(World world) {
-        world.removeEntity(tower);
-
     }
 
     @Override
